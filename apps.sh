7fcb938b9e55260cb24d639eab44bb7031706091
James <james@pop-os.localdomain>
2023-05-14 16:11:54 +1000

    added apps

---
+++ apps.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/bash
+
+# cli
+sudo apt install -y \
+	tmux \
+	fzf \
+	tldr \
+	neovim \
+	htop \
+	sox \
+	gh \
+	nmap \
+	zsh
+
+# gui 
+sudo apt install -y \
+	transmission \
+	vlc \
+	gnome-tweaks \
+	google-chrome-stable \
+	steam \
+	dbeaver
